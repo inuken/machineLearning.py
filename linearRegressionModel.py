3c1b9557491e7897e3e113d43f578800019ecb09
INUKEN <inukenta@gmail.com>
2015-06-26 18:32:19 +0900

    complete

--- linearRegressionModel.py
+++ linearRegressionModel.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 
 X = np.array([0.02, 0.12, 0.19, 0.27, 0.42, 0.51, 0.64, 0.84, 0.88, 0.99])
-t = np.array([0.05, 0.87, 0.94, 0.92, 0.54, -0.11, -0.78, -0.89, -0.79, -0.04])
+t = np.array([0.05, 0.87, 0.94, 0.92, 0.54, -0.11, -0.78, -0.79, -0.89, -0.04])
 d = 7
 r = 0.001
 
@@ -16,7 +16,7 @@ def phi(x):
     return [1 if i == 0 else x ** i for i in range(d)]
 
 PHI = np.array([phi(x) for x in X])
-w = np.dot(np.linalg.inv(r * np.identity(d) + np.dot(PHI.T, PHI)), np.dot(PHI.T, t))
+w = np.linalg.solve(np.dot(PHI.T, PHI), np.dot(PHI.T, t))
 
 def f(w, x):
     return np.dot(w, phi(x))
